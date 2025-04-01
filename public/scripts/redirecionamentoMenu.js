a58f9e17dc6fc62fc8719228c45403135a160159
henriquedandrade <henrique.barros@sptech.school>
2025-03-31 21:47:58 -0300

    Começando tela de Alertas

---
+++ public/scripts/redirecionamentoMenu.js
@@ -0,0 +1,36 @@
+function btn_rt(){
+    window.location = './dash_realTime.html';
+}
+
+function btn_hist(){
+    window.location = './dash_analiseHistorico.html'
+}
+
+function btn_geral(){
+    window.location = './dash_analiseGeral.html';
+}
+
+function btn_func() {
+    window.location = './cadastroFuncionario.html';
+}
+
+function btn_disp() {
+    window.location = './cadastroModelo.html';
+}
+
+function btn_maquina() {
+    window.location = './cadastroMaquina.html';
+}
+
+function btn_endereco(){
+    window.location = './cadastroEndereco.html';
+}
+
+function btn_alerta(){
+    window.location = './alertas.html';
+
+}
+
+function sair(){
+    window.location = '../index.html';
+}
