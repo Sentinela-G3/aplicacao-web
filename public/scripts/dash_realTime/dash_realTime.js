66cdec37026e4ff8a7ce53b171434c90c73c2e3c
vinicius201002 <vinicius.opedroso@sptech.school>
2025-04-20 18:41:45 -0300

    feat: tela de monitoramento em tempo real

---
+++ public/scripts/dash_realTime/dash_realTime.js
@@ -0,0 +1,21 @@
+// // Verificando sessão do usuário
+// if (!sessionStorage.idEmpresa || !sessionStorage.idUsuario || !sessionStorage.email || !sessionStorage.tipoUsuario || !sessionStorage.nomeUsuario) {
+//     alert("Sua sessão expirou! Logue-se novamente.");
+//     window.location.href = "../login.html";
+// }
+
+// const usuario = {
+//     idEmpresa: sessionStorage.idEmpresa,
+//     idUsuario: sessionStorage.idUsuario,
+//     email: sessionStorage.email,
+//     tipoUsuario: sessionStorage.tipoUsuario,
+//     nomeUsuario: sessionStorage.nomeUsuario
+// };
+
+// // Plotando nome no menu lateral
+// const b_nome_usuario = document.getElementById("nome_usuario");
+// b_nome_usuario.innerHTML = usuario.nomeUsuario;
+
+function carregarSetor() {
+    
+}
