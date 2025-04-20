2a3d6fd12dfbf365bcfd474f4c58a4b86ed73977
vinicius201002 <vinicius.opedroso@sptech.school>
2025-04-20 17:52:56 -0400

    fix: consertando model para adaptar ao banco de dados

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,9 +1,8 @@
 var database = require("../database/config")
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function autenticar(): ", email, senha)
     var instrucaoSql = `
-         SELECT idUsuario, colaborador.nome as nome, email, tipoUsuario, idEmpresa, empresa.status FROM colaborador join empresa on idEmpresa = fkEmpresa WHERE email = '${email}' AND senha = SHA2('${senha}', 256);
+         SELECT id_usuario, colaborador.nome as nome, email, tipo, id_empresa, empresa.status FROM colaborador join empresa on id_empresa = fk_colaborador_empresa WHERE email = '${email}' AND senha = SHA2('${senha}', 256);
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -11,7 +10,7 @@ function autenticar(email, senha) {
 
 function cadastrar(nome, email, telefone, senha, fkEmpresa, tipoUsuario) {
     var instrucaoSql = `
-        INSERT INTO colaborador (fkEmpresa, nome, email, telefone, senha, tipoUsuario, dataCriacao) 
+        INSERT INTO colaborador (fk_colaborador_empresa, nome, email, telefone, senha, tipo, data_criacao) 
         VALUES (${fkEmpresa}, '${nome}', '${email}', '${telefone}', SHA2('${senha}', 256), ${tipoUsuario}, NOW());
     `;
     
@@ -22,7 +21,7 @@ function cadastrar(nome, email, telefone, senha, fkEmpresa, tipoUsuario) {
 
 function listarPorEmpresa(fkEmpresa) {
     var instrucaoSql = `
-        SELECT idUsuario, nome, tipoUsuario, DATE_FORMAT(dataCriacao, '%Y-%m-%d') AS dataCriacao
+        SELECT id_usuario, nome, tipo, DATE_FORMAT(data_criacao, '%Y-%m-%d') AS dataCriacao
         FROM usuario
         WHERE fkEmpresa = ${fkEmpresa};
     `;
@@ -35,7 +34,7 @@ function deletar(idFuncionario) {
     var instrucaoSql = `
        DELETE
         FROM usuario
-        WHERE idUsuario = ${idFuncionario};
+        WHERE id_usuario = ${idFuncionario};
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -45,8 +44,8 @@ function deletar(idFuncionario) {
 function atualizar(idFuncionario, nome, email, telefone, tipoUsuario) {
     var instrucaoSql = `
         UPDATE usuario 
-        SET nome = '${nome}', email = '${email}', telefone = '${telefone}', tipoUsuario = ${tipoUsuario} 
-        WHERE idUsuario = ${idFuncionario};
+        SET nome = '${nome}', email = '${email}', telefone = '${telefone}', tipo = ${tipoUsuario} 
+        WHERE id_usuario = ${idFuncionario};
     `;
     
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -55,7 +54,7 @@ function atualizar(idFuncionario, nome, email, telefone, tipoUsuario) {
 
 
 function buscarPorId(id) {
-    var instrucaoSql = `SELECT * FROM usuario where idUsuario = ${id}`;
+    var instrucaoSql = `SELECT * FROM usuario where id_usuario = ${id}`;
     return database.executar(instrucaoSql);
 }
 
