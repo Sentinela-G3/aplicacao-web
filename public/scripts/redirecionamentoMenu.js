334ffbcaecc5778a32df4cedfcc1dc84b141c014
henriquedandrade <henrique.barros@sptech.school>
2025-04-25 10:34:07 -0300

    Gerar Menu Lateral pelo JS

--- public/scripts/redirecionamentoMenu.js
+++ public/scripts/redirecionamentoMenu.js
@@ -1,3 +1,67 @@
+function gerarMenuLateral(){
+    const div_lateral = document.getAnimations('body_dashRealTime')
+
+    div_lateral.innerHTML = `
+                    <div class="perfil">
+                            <div class="logo">
+                                <img src="../assets/img/svgLogo.svg">
+                                <span>Sentinela</span>
+                            </div>
+                            <div class="imagem_perfil">
+                                <img id="fotoPerfil_menu">
+                            </div>
+                            <div class="texto_perfil">
+                                <span>
+                                    Olá,
+                                    <b>${sessionStorage.nomeUsuario}!</b>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="btn_nav">
+                            <div class="btn_dash">
+                                <li>
+                                    <button onclick="btn_rt()">Análise em tempo real</button>
+                                </li>
+                                <li>
+                                    <button onclick="btn_hist()">Análise histórica</button>
+                                </li>
+                                <li>
+                                    <button onclick="btn_geral()">Análise geral</button>
+                                </li>
+                                <li>
+                                    <button onclick="btn_endereco()">Gerenciamento de Endereços</button>
+                                </li>
+                                <li>
+                                    <button onclick="btn_func()">Gerenciamento de Funcionário</button>
+                                </li>
+                                <li>
+                                    <button onclick="btn_disp()">Gerenciamento de Modelo</button>
+                                </li>
+                                <li>
+                                    <button onclick="btn_maquina()">Gerenciamento de Maquina</button>
+                                </li>
+                                <li>
+                                    <button onclick="btn_alerta()" class="agora">Alertas</button>
+                                </li>
+                            </div>
+                            <div class="btn_sair">
+                                <button onclick="sair()">Sair
+                                    <img src="../assets/img/sair.png" alt="Sair">
+                                </button>
+                            </div>
+                        </div>`;
+                    
+    const foto = document.getElementById('fotoPerfil_menu')
+
+    if(sessionStorage.fotoPerfil){
+        foto.scr = "../assets/img/img_perfil_nav.jpg"
+    }else{
+        foto.scr = `../imagens_de_perfil/${sessionStorage.fotoPerfil}`
+    }
+
+                        
+}
+
 function btn_rt(){
     window.location = './dash_realTime.html';
 }
@@ -40,3 +104,5 @@ function sair(){
     window.location = '../index.html';
     sessionStorage.clear();
 }
+
+gerarMenuLateral()
