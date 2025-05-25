b3e774dc514dc1f5f2c0cb81a812d4effff3e85d
AaronCutipa <aaron.canaviri@sptech.school>
2025-05-25 00:18:25 -0300

    Finalizando construção da dashboard real time

--- public/scripts/dash_realTime/dash_realTime.js
+++ public/scripts/dash_realTime/dash_realTime.js
@@ -1,7 +1,7 @@
-if (!sessionStorage.idEmpresa || !sessionStorage.idUsuario || !sessionStorage.email || !sessionStorage.tipoUsuario || !sessionStorage.nomeUsuario) {
-    alert("Sua sessão expirou! Logue-se novamente.");
-    window.location.href = "../login.html";
-}
+// if (!sessionStorage.idEmpresa || !sessionStorage.idUsuario || !sessionStorage.email || !sessionStorage.tipoUsuario || !sessionStorage.nomeUsuario) {
+//     alert("Sua sessão expirou! Logue-se novamente.");
+//     window.location.href = "../login.html";
+// }
 
 
 const BASE_URL = window.location.hostname === "localhost"
