c785c422f5b33c452d2fb9375eb2fdc13065a705
Victorslima13 <victor.slima@sptech.school>
2025-06-10 21:21:42 -0300

    correção do nome do bucket

--- src/controllers/bucketController.js
+++ src/controllers/bucketController.js
@@ -1,9 +1,8 @@
 const { buscarArquivoDoS3 } = require("../services/aws/s3Service");
 
 async function buscarDados4Anos(req, res) {
-  console.log(req)
   const empresa = req.params.empresa;
-  const modelo = req.params.modelo;
+  const modelo = req.params.nomeModelo;
   console.log("Empresa: ", empresa)
   console.log("Empresa: ", modelo)
   
@@ -11,7 +10,7 @@ async function buscarDados4Anos(req, res) {
   console.log(nomeArquivo)
 
   try {
-    const conteudo = await buscarArquivoDoS3("clientbuckets3-spt", nomeArquivo);
+    const conteudo = await buscarArquivoDoS3("sentinela-client-bucket", nomeArquivo);
     res.status(200).json(JSON.parse(conteudo));
   } catch (err) {
     console.error("Erro ao buscar no S3:", err);
